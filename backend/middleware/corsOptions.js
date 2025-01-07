c05101087feba3b75da2a891f08ffde3df5c9583
Elemo9 <59938157+Elemo9@users.noreply.github.com>
2025-01-07 13:13:16 +0100

    Update corsOptions.js

--- backend/middleware/corsOptions.js
+++ backend/middleware/corsOptions.js
@@ -1,26 +1,8 @@
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
-require('dotenv').config();
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'https://adiremarket.com',
+  methods: ['POST', 'GET', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
 
-const dbPath = path.resolve(__dirname, '../', process.env.DATABASE_PATH || './db/emails.db');
-
-const db = new sqlite3.Database(dbPath, (err) => {
-  if (err) {
-    console.error("Error connecting to database:", err.message);
-  } else {
-    console.log('Connected to the SQLite database.');
-    db.run(`CREATE TABLE IF NOT EXISTS emails (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      email TEXT UNIQUE NOT NULL,
-      created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-    )`, (err) => {
-      if (err) {
-        console.error("Error creating table:", err.message);
-      } else {
-        console.log("Emails table ensured with created_at timestamp.");
-      }
-    });
-  }
-});
-
-module.exports = db;
+module.exports = corsOptions;
